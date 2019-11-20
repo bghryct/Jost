cd7110a31ac8cdd6d14586b33e14c40902600df7
mirko velimirovic <bghryct@gmail.com>
2019-11-20 14:34:42 -0500

    cleaned massively, wrote build script, ran QA

--- build.sh
+++ build.sh
@@ -8,7 +8,8 @@ set -e
 
 echo "Generating VFs"
 # mkdir -p ./fonts/variable
-fontmake -m source/jost.designspace -o variable --round-instances -a --output-path source/vf/jost\[ital,wght\].ttf
+echo "Generating VFs"
+fontmake -g source/jost.glyphs -o variable --round-instances -a --output-path source/vf/jost\[slnt,wght\].ttf
 
 echo "Removing Build UFOS"
 
@@ -16,21 +17,9 @@ rm -rf master_ufo/ instance_ufo/
 
 echo "Build UFOS Removed"
 
-echo "Post processing"
+echo "vf cleaning"
 
-ttfs=$(ls fonts/static/*.ttf)
-echo $ttfs
-for ttf in $ttfs
-do
-	gftools fix-dsig -f $ttf;
-	gftools fix-nonhinting $ttf "$ttf.fix";
-	mv "$ttf.fix" $ttf;
-done
-echo "fixed nonhinting ttfs as well as DSIG"
-
-rm ./fonts/static/*backup*.ttf
-
-vfs=$(ls ./fonts/variable/*.ttf)
+vfs=$(ls ./source/vf/*.ttf)
 for vf in $vfs
 do
 	gftools fix-dsig -f $vf;
@@ -38,18 +27,37 @@ do
 	mv "$vf.fix" $vf;
 	ttx -f -x "MVAR" $vf; # Drop MVAR. Table has issue in DW
 	rtrip=$(basename -s .ttf $vf)
-	new_file=./fonts/variable/$rtrip.ttx;
+	new_file=./source/vf/$rtrip.ttx;
 	rm $vf;
 	ttx $new_file
-	rm ./fonts/variable/*.ttx
+	rm ./source/vf/*.ttx
 done
-rm ./fonts/variable/*backup*.ttf
+rm ./source/vf/*backup*.ttf
 
-echo "fix vf meta? ok let's try that. I'm trying to fix VF meta"
-gftools fix-vf-meta $vfs;
-for vf in $vfs
-do
-	mv "$vf.fix" $vf;
-done
+echo "end vf cleaning"
 
-echo "Post processing complete"
+fontbakery check-googlefonts ./source/vf/*.ttf --ghmarkdown jost_problems.md
+# echo "Post processing"
+
+# ttfs=$(ls fonts/static/*.ttf)
+# echo $ttfs
+# for ttf in $ttfs
+# do
+# 	gftools fix-dsig -f $ttf;
+# 	gftools fix-nonhinting $ttf "$ttf.fix";
+# 	mv "$ttf.fix" $ttf;
+# done
+# echo "fixed nonhinting ttfs as well as DSIG"
+
+# rm ./fonts/static/*backup*.ttf
+
+
+
+# echo "fix vf meta? ok let's try that. I'm trying to fix VF meta"
+# gftools fix-vf-meta $vfs;
+# for vf in $vfs
+# do
+# 	mv "$vf.fix" $vf;
+# done
+
+# echo "Post processing complete"
